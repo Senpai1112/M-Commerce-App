b5f5a08ba14e9334deddc1e60ae27dbdf962b664
youssab <youssabyasser@gmail.com>
2025-02-10 20:25:03 +0200

    Shopping Cart UI and plus and minus Buttons added and logic is tested - need to add delete cell feature

---
+++ Shopify/Shopify/View/Shopping Cart/ShoppingCartTableViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  ShoppingCartTableViewCell.swift
+//  Shopify
+//
+//  Created by Yasser Yasser on 10/02/2025.
+//
+
+import UIKit
+
+protocol ShoppingCartTableViewCellDelegate{
+    func removeCell(at indexPath: IndexPath)
+}
+
+class ShoppingCartTableViewCell: UITableViewCell {
+    
+
+    var delegate : ShoppingCartTableViewCellDelegate?
+    private var indexPath :IndexPath?
+    
+    @IBOutlet weak var productQuantaty: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        initUI()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func initUI(){
+        plusButton.layer.cornerRadius = 20
+        plusButton.clipsToBounds = true
+        
+        minusButton.layer.cornerRadius = 20
+        minusButton.clipsToBounds = true
+
+        productImage.layer.cornerRadius = 50
+        
+        productPrice.textColor = .systemRed
+    }
+    
+    func configure(with indexPath :IndexPath){
+        self.indexPath = indexPath
+    }
+    
+    @IBAction func tabPlusButton(_ sender: Any) {
+        var num = Int(productQuantaty.text!)
+        num! += 1
+        productQuantaty.text = num!.codingKey.stringValue
+    }
+    @IBAction func tabMinusButton(_ sender: Any) {
+        let num = Int(productQuantaty.text!)
+        if var num = num {
+            if num > 1{
+                num -= 1
+                productQuantaty.text = num.codingKey.stringValue
+            }
+            else{
+                delegate?.removeCell(at: self.indexPath!)
+            }
+        }
+        
+    }
+}
---
+++ Shopify/Shopify/View/Shopping Cart/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Shopify
+//
+//  Created by Yasser Yasser on 09/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var checkoutButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names : [String] = ["youssab" , "yasser" , "youssef" , "ziad" , "mai" , "andrew" , "hellana" ,"mohsen" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initNib()
+        initUI()
+    }
+
+    func initNib(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        let nib = UINib(nibName: "ShoppingCartTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "cell")
+    }
+    func initUI(){
+        checkoutButton.layer.cornerRadius = checkoutButton.frame.height / 2
+        checkoutButton.layer.cornerCurve = .continuous
+        checkoutButton.clipsToBounds = true
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate , UITableViewDataSource , ShoppingCartTableViewCellDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if names.count == 0 {
+            return 0
+        }else{
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ShoppingCartTableViewCell
+        cell.delegate = self
+        cell.productName.text = names[indexPath.row]
+        cell.configure(with: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func removeCell(at indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Deleting", message: "Do you want to delete \(names[indexPath.row])", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .destructive) { _ in
+            self.names.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        })
+        alert.addAction(UIAlertAction(title: "Cancle", style: .default, handler: {_ in }))
+        self.present(alert, animated: true)
+    }
+}
+
