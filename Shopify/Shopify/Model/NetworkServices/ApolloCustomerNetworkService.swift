18369b6a0e091d260abb64f2a2bc8f43988947c1
youssab <youssabyasser@gmail.com>
2025-02-24 09:48:06 +0200

    Apollo Network service is now singleton

--- Shopify/Shopify/Model/NetworkServices/ApolloCustomerNetworkService.swift
+++ Shopify/Shopify/Model/NetworkServices/ApolloCustomerNetworkService.swift
@@ -10,27 +10,8 @@ import Apollo
 import MyApi
 
 class ApolloCustomerNetworkService{
-    static let shared = ApolloCustomerNetworkService()
-    
-    private let url = URL(string: "https://itp-newcapital-ios3.myshopify.com/api/2025-01/graphql")!
-    
-    private let store = ApolloStore()
-    
-    private lazy var networkTransport: RequestChainNetworkTransport = {
-        return RequestChainNetworkTransport(
-            interceptorProvider: DefaultInterceptorProvider(store: store),
-            endpointURL: url,
-            additionalHeaders: [
-                "X-Shopify-Storefront-Access-Token": "fab79f9e9f027819aba7284b60fdb5f3",
-                "Content-Type": "application/json"
-            ]
-        )
-    }()
-    
-    private(set) lazy var apollo = ApolloClient(networkTransport: networkTransport, store: store)
-    
     static func fetchCustomerDetailsFromModel(token : String?,completion: @escaping (Result<GraphQLResult<GetCustomerDetailsQuery.Data>, Error>) -> Void) {
-        ApolloCustomerNetworkService.shared.apollo.fetch(query: GetCustomerDetailsQuery(token: token!),cachePolicy: .fetchIgnoringCacheData) { result in
+        ApolloNetwokService.shared.apollo.fetch(query: GetCustomerDetailsQuery(token: token!),cachePolicy: .fetchIgnoringCacheData) { result in
             switch result {
             case .success(let graphQLResult):
                 completion(.success(graphQLResult))
--- Shopify/Shopify/Model/NetworkServices/ApolloProductsNetwokService.swift
+++ Shopify/Shopify/Model/NetworkServices/ApolloProductsNetwokService.swift
@@ -10,29 +10,8 @@ import MyApi
 
 class ApolloProductsNetwokService {
     
-    static let shared = ApolloProductsNetwokService()
-    
-    private let url = URL(string: "https://itp-newcapital-ios3.myshopify.com/api/2025-01/graphql")!
-    
-    // Create Apollo Store (for caching)
-    private let store = ApolloStore()
-    
-    // Custom Network Transport with Headers
-    private lazy var networkTransport: RequestChainNetworkTransport = {
-        return RequestChainNetworkTransport(
-            interceptorProvider: DefaultInterceptorProvider(store: store),
-            endpointURL: url,
-            additionalHeaders: [
-                "X-Shopify-Storefront-Access-Token": "fab79f9e9f027819aba7284b60fdb5f3",
-                "Content-Type": "application/json"
-            ]
-        )
-    }()
-    
-    private(set) lazy var apollo = ApolloClient(networkTransport: networkTransport, store: store)
-    
     func fetchCollections(completion: @escaping (Result<GraphQLResult<GetAllCollectionsQuery.Data>, Error>) -> Void) {
-        ApolloProductsNetwokService.shared.apollo.fetch(query: GetAllCollectionsQuery()) { result in
+        ApolloNetwokService.shared.apollo.fetch(query: GetAllCollectionsQuery()) { result in
             switch result {
             case .success(let graphQLResult):
                 completion(.success(graphQLResult))
@@ -44,7 +23,7 @@ class ApolloProductsNetwokService {
     
     func fetchProducts(query: String, completion: @escaping (Result<GraphQLResult<ProductsQuery.Data>, Error>) -> Void) {
         let productsQuery = ProductsQuery(query: query)
-        ApolloProductsNetwokService.shared.apollo.fetch(query: productsQuery) { result in
+        ApolloNetwokService.shared.apollo.fetch(query: productsQuery) { result in
             switch result {
             case .success(let graphQLResult):
                 completion(.success(graphQLResult))
@@ -55,7 +34,7 @@ class ApolloProductsNetwokService {
     }
     
      func fetchCustomerOrders(token : String,completion: @escaping (Result<GraphQLResult<CustomerOrdersQuery.Data>, Error>) -> Void) {
-        ApolloAddressesNetwokService.shared.apollo.fetch(query: CustomerOrdersQuery(token: token),cachePolicy: .fetchIgnoringCacheData) { result in
+         ApolloNetwokService.shared.apollo.fetch(query: CustomerOrdersQuery(token: token),cachePolicy: .fetchIgnoringCacheData) { result in
             switch result {
             case .success(let graphQLResult):
                 completion(.success(graphQLResult))
